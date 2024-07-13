d6959d0858d2714d09c4bce1d3eeddaebaaaa9a5
kuyaLor <161716704+kuyaLor@users.noreply.github.com>
2024-07-13 23:09:44 +0800

    change the way undefineds are handled

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let caller_btn = document.querySelector('#caller-btn');
+let caller_btn = document.querySelector("#caller-btn");
 
-caller_btn.addEventListener('click', function() {fetch("https://v2.jokeapi.dev/joke/Programming")
-    .then(function(response) {
+caller_btn.addEventListener("click", function () {
+  fetch("https://v2.jokeapi.dev/joke/Programming")
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
+      let joke = data.joke;
 
-        let joke = data.joke;
-
-        if(joke === undefined){
-            // append an image element to the parent div
-            let img = document.createElement('img');
-            img.src = "https://i.pinimg.com/564x/57/c2/c1/57c2c16decf4717306c60c159e3faf5b.jpg";
-            img.alt = "joke";
-            document.querySelector('.joke').innerHTML = "";
-            document.querySelector('.joke').appendChild(img);
-        }
-        else{
-            document.querySelector('.joke').innerHTML = joke;
-        }
-        
+      if (joke === undefined) {
+        joke = "Mahilig kasi ako mag online games nuon haha";
+      }
+      document.querySelector(".joke").innerHTML = joke;
     });
 });
